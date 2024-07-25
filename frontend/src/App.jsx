e4f777792d757f2841bcd70a03f567ad4e002da3
tinoj <tino.korpelainen@aalto.fi>
2024-07-25 21:53:21 +0300

    Deleted unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,42 +2,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // Set the date we're counting down to
-  var countDownDate = new Date("Dec 6, 2024 12:00:00").getTime();
+	// Set the date we're counting down to
+	var countDownDate = new Date("Dec 6, 2024 12:00:00").getTime();
 
-  // Update the count down every 1 second
-  var x = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
+	// Update the count down every 1 second
+	var x = setInterval(function () {
+		// Get today's date and time
+		var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+		// Find the distance between now and the count down date
+		var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    document.getElementById("printCountdown").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+		// Output the result in an element with id="demo"
+		document.getElementById("printCountdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is over, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.write("Äänestys alkoi!");
-    }
-  }, 1000);
-  return (
-    <div>
-      <p>Äänestys alkaa</p>
-      <br></br>
-      <p id="printCountdown"></p>
-    </div>
-  );
+		// If the count down is over, write some text
+		if (distance < 0) {
+			clearInterval(x);
+			document.write("Äänestys alkoi!");
+		}
+	}, 1000);
+	return (
+		<div>
+			<p>Äänestys alkaa</p>
+			<br></br>
+			<p id="printCountdown"></p>
+		</div>
+	);
 }
 export default App;
