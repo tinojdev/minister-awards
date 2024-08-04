8803137221168db432b15f3e90ed8ea41fc3b5f6
tinoj <tino.korpelainen@aalto.fi>
2024-08-04 18:47:56 +0300

    Updated urls

--- backend/minister_awards/settings.py
+++ backend/minister_awards/settings.py
@@ -48,6 +48,7 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 MEDIA_ROOT = os.getenv("DJANGO_MEDIA_ROOT", None) or BASE_DIR / "media"
 STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", None) or BASE_DIR / "static"
+STATIC_URL = os.getenv("DJANGO_STATIC_URL", None) or "static/"
 
 # Application definition
 
@@ -148,7 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = ["static/", "/var/www/minister-awards/static/"]
 MEDIA_URL = "/"
 
 # Default primary key field type
