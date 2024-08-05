26e074430f527c8cdb0855cedf62719d52457059
tinoj <tino.korpelainen@aalto.fi>
2024-08-05 15:18:14 +0300

    Added compression of new images

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,16 @@
+from typing import Iterable
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
 from django.db import models
+
+
+def compress_image(image: models.ImageField) -> models.ImageField:
+    img = Image.open(image)
+    img_io = BytesIO()
+    img.save(img_io, "JPEG", quality=70)
+    new_image = File(img_io, name=image.name)
+    return new_image
 
 
 class Category(models.Model):
@@ -17,6 +29,11 @@ class Nomination(models.Model):
         Category, related_name="nominations", on_delete=models.CASCADE
     )
     image = models.ImageField(upload_to="nomination_images", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        new_image = compress_image(self.image)
+        self.image = new_image
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
