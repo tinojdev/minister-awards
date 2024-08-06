6b1b58ad1ecdd565ba53e7c0f9d8e1be385133d1
tinoj <tino.korpelainen@aalto.fi>
2024-08-06 19:23:11 +0300

    Added imagekit to backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,16 +1,10 @@
 from typing import Iterable
 from PIL import Image
 from io import BytesIO
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 from django.core.files import File
 from django.db import models
-
-
-def compress_image(image: models.ImageField) -> models.ImageField:
-    img = Image.open(image)
-    img_io = BytesIO()
-    img.save(img_io, "JPEG", quality=70)
-    new_image = File(img_io, name=image.name)
-    return new_image
 
 
 class Category(models.Model):
@@ -29,11 +23,12 @@ class Nomination(models.Model):
         Category, related_name="nominations", on_delete=models.CASCADE
     )
     image = models.ImageField(upload_to="nomination_images", blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        new_image = compress_image(self.image)
-        self.image = new_image
-        super().save(*args, **kwargs)
+    image_thumbnail = ImageSpecField(
+        source="image",
+        format="JPEG",
+        processors=[ResizeToFill(300, 200)],
+        options={"quality": 60},
+    )
 
     def __str__(self):
         return self.name
