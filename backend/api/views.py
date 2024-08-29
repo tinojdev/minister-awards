d058306315dcdd898af358e64fbffef3cb9a7805
tinoj <tino.korpelainen@aalto.fi>
2024-08-29 20:48:34 +0300

    Bugfix of bugfix

--- backend/api/views.py
+++ backend/api/views.py
@@ -114,7 +114,7 @@ class VoterList(views.APIView):
 
         if should_return_points:
             voters = Voter.objects.all().annotate(
-                total_points=Sum("nominations__votes__weight", 0)
+                total_points=Sum("nominations__votes__weight", default=0)
             )
 
             serializer = VoterSerializer(voters, many=True)
