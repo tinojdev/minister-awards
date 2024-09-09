f292976a40e2420c0355cb1dd1596a9c7cdb2b24
Santtu777 <santeri.pentinen@gmail.com>
2024-09-09 13:23:14 +0300

    Update

--- frontend/src/components/CategoryLinkTest.jsx
+++ frontend/src/components/CategoryLinkTest.jsx
@@ -18,11 +18,11 @@ const responsive = {
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 4
+    items: 3
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 4
+    items: 2.5
   }
 };
 
